585b7d9e858d4c1e52ec283ae9f70f6d560b6455
Raphael Scursoni <ra.scursoni@gmail.com>
2021-02-20 09:49:23 -0300

    Personalizando com Equals a comparacao de obj

---
+++ GetHashCode-Equals-Personalizados/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using GetHashCode_Equals_Personalizados.Entities;
+
+namespace GetHashCode_Equals_Personalizados {
+    class Program {
+        static void Main(string[] args) {
+
+            Client a = new Client { Name = "Maria", Email = "maria@gmai.com" };
+            Client b = new Client { Name = "Alex", Email = "alex@gmai.com" };
+
+            Console.WriteLine(a.Equals(b)); // Compara pela referência
+            Console.WriteLine(a == b); // Compara objetos na memória, CONTEÚDO
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+
+        }
+    }
+}
